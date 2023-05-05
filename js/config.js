9be4132ada2af05303795ff5fc917c2430d46f45
Shubham <119788747+Shubham1322c@users.noreply.github.com>
2023-05-05 10:28:37 +0530

    add api

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-export const API_KEY = 'Your_API_KEY'; // You can get your key from "https://openweathermap.org/"
+export const API_KEY = '349216cea78e036f21a1ea7fa92dd65a'; // You can get your key from "https://openweathermap.org/"
